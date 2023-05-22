e479c0705f808bfeee9b700dcd70172d2e2cdc75
Alexis-Contente <alexis.contente@gmail.com>
2023-05-22 16:50:26 +0200

    main form created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,46 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+      <div className={styles.inputs__main}>
+        <input className={styles.search} type="search" placeholder='Search contact' />
+        <button className={styles.add}>ADD CONTACT</button>
+      </div>
+
+      <div className={styles.book}>
+        <div className={styles.card}>
+          <p className={styles.names}>FIRSTNAME LASTNAME</p>
+          <p className={styles.email}>EXAMPLE@EMAIL.COM</p>
+          <p className={styles.birth}>00/00/0000</p>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.popup}>
+
+        <div className={styles.close}>X</div>
+
+        <div className={styles.form}>
+          <div className={styles.inputs__form}>
+            <input className={styles.add__firstname} type="text" placeholder='Firstname' />
+            <input className={styles.add__lastname} type="text" placeholder='Lastname'/>
+            <input className={styles.add__email} type="text" placeholder='Email'/>
+            <input className={styles.add__birth} type="text" placeholder='Date of birth'/>
+          </div>
+
+          <div className={styles.add__informations}>
+            <input className={styles.input__informations} type="text" placeholder='Additional informations' />
+          </div>
+
+          <div className={styles.buttons}>
+            <button className={styles.save}>Save</button>
+            <button className={styles.delete}>Delete</button>
+          </div>
+        </div>
+
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
