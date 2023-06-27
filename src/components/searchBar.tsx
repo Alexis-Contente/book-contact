cb8410688bf9f053e7e79ece952b7e487c7f4aaf
Alexis-Contente <alexis.contente@gmail.com>
2023-06-27 14:05:52 +0200

    correction

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -2,20 +2,25 @@
 
 // IMPORTS
 import styles from '../app/page.module.css';
+import { useState } from 'react';
 
 type Props = {
   toggleDisplayForm: () => void;
 };
+
 // export default function SearchBar<Props>({toggleDisplayForm}) {
 export default function SearchBar(props: Props) {
   const { toggleDisplayForm } = props;
 
   return (
-    <div className={styles.inputs__main}>
-      <input className={styles.search} type="search" placeholder="Search contact" />
+    <form className={styles.inputs__main}>
+      <input className={styles.search} placeholder="Search contact" type="text" />
+      <button className={styles.add} type="submit">
+        Rechercher
+      </button>
       <button className={styles.add} onClick={toggleDisplayForm}>
         ADD CONTACT
       </button>
-    </div>
+    </form>
   );
 }
